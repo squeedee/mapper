8cfa6c6b7b2a4a3b3f124b2049474bb33446ce53
Rasheed Abdul-Aziz <squeedee@gmail.com>
2014-10-18 12:53:33 -0400

    Remove unused pad function

--- src/mapper/util.clj
+++ src/mapper/util.clj
@@ -2,7 +2,3 @@
 
 (defn coordinates [width index]
   [(rem index width) (quot index width)])
-
-(defn pad [coll-a coll-b]
-  "pad coll-a with nils to match coll-b's cardinality"
-  (concat coll-a (repeat (- (count coll-b) (count coll-a)) nil)))
