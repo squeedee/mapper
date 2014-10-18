c4569652d06e74bcc459f1c39ea8e8f6ba563839
Rasheed Abdul-Aziz <squeedee@gmail.com>
2014-10-18 12:53:31 -0400

    simpler arity syntax for diff

--- src/mapper/util/diff.clj
+++ src/mapper/util/diff.clj
@@ -2,12 +2,14 @@
   (:require [mapper.util :refer :all]
             [mapper.core :refer :all]))
 
-(defn diff [[width height] map-a map-b]
-  "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same.
+(defn diff
+  ([map-a-coll map-b-coll]
+   (map = map-a-coll map-b-coll))
+  ([[width height] map-a map-b]
+   "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same.
   E.g.: diff [2 2] '(:a :b :c :d) '(:a :b :c :z) => '(true true true false)"
-  (let [map-a-coll (map-as-list [width height] map-a)
-        map-b-coll (map-as-list [width height] map-b)]
-    (map = map-a-coll map-b-coll)))
+   (diff (map-as-list [width height] map-a)
+         (map-as-list [width height] map-b))))
 
 (defn diff? [dimensions map-a map-b]
   "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same.
