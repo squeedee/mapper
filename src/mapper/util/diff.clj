638c2cabb22a24f59684bc3fd58f4015047dde52
Rasheed Abdul-Aziz <squeedee@gmail.com>
2014-10-18 12:53:32 -0400

    introduce data/diff

--- src/mapper/util/diff.clj
+++ src/mapper/util/diff.clj
@@ -1,18 +1,25 @@
 (ns mapper.util.diff
   (:require [mapper.util :refer :all]
-            [mapper.core :refer :all]))
+            [mapper.core :refer :all]
+            [clojure.data :as data]))
+
+(defn left [diff]
+  (diff 0))
+
+(defn right [diff]
+  (diff 1))
+
+(defn intersection [diff]
+  (diff 2))
 
 (defn diff
-  ([map-a-coll map-b-coll]
-   (map = map-a-coll map-b-coll))
   ([[width height] map-a map-b]
-   "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same.
-  E.g.: diff [2 2] '(:a :b :c :d) '(:a :b :c :z) => '(true true true false)"
-   (diff (map-as-list [width height] map-a)
+   "A utility for testing map equality via clojure.data/diff. Converts map functions to sequences for diffing."
+   (data/diff (map-as-list [width height] map-a)
          (map-as-list [width height] map-b))))
 
 (defn diff? [dimensions map-a map-b]
-  "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same.
-  E.g.: diff [2 2] '(:a :b :c :d) '(:a :b :c :z) => false
-        diff [2 2] '(:a :b :c :d) '(:a :b :c :d) => true"
-  (every? true? (diff dimensions map-a map-b)))
+  "A utility for testing map equality. Compares map-a and map-b elementwise and returns true if they're the same."
+  (let [diff-result (diff dimensions map-a map-b)]
+    (and (nil? (left diff-result))
+         (nil? (right diff-result)))))
--- test/mapper/util/diff_test.clj
+++ test/mapper/util/diff_test.clj
@@ -6,8 +6,18 @@
 (def map1 (create-map 2 [:a :b :c :d :e :f]))
 (def map2 (create-map 2 [:c :b :a :d :e :a]))
 
-(expect '(false true false true true false)
+(expect [[:a nil :c nil nil :f] [:c nil :a nil nil :a] [nil :b nil :d :e]]
         (diff [2 3] map1 map2))
+
+(expect [:a nil :c nil nil :f]
+        (left (diff [2 3] map1 map2)))
+
+(expect [:c nil :a nil nil :a]
+        (right (diff [2 3] map1 map2)))
+
+(expect [nil :b nil :d :e]
+        (intersection (diff [2 3] map1 map2)))
+
 
 (expect false
         (diff? [2 3] map1 map2))
